a7171119d702cb90c80f96b5d9dd74343160165f
Nikcet <mstislav.kr@yandex.ru>
2022-07-20 01:29:52 +0300

    cors

--- backend/utils/cors.js
+++ backend/utils/cors.js
@@ -4,21 +4,18 @@ const { allowedUrls } = require('./allowedUrls');
 
 module.exports.cors = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
-  console.log(requestHeaders);
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
   const { origin } = req.headers;
-  console.log(origin);
   const { method } = req;
-  console.log(method);
 
   if (allowedUrls.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.set('Access-Control-Allow-Origin', origin);
   }
 
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Credentials', true);
+    res.set('Access-Control-Allow-Headers', requestHeaders);
+    res.set('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.set('Access-Control-Allow-Credentials', 'true');
     // return res.end();
   }
 

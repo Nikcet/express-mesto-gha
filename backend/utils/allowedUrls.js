093d87a16aaca91ac91a40c40bea90bbcff28e4f
Nikcet <mstislav.kr@yandex.ru>
2022-07-19 15:32:38 +0300

    Настроил cors

---
+++ backend/utils/allowedUrls.js
@@ -0,0 +1,10 @@
+module.exports.allowedUrls = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+  'localhost:3001',
+  'http://mesto.edu.nomoredomains.xyz',
+  'http://api.mesto.edu.nomoredomains.xyz',
+  'https://mesto.edu.nomoredomains.xyz',
+  'https://api.mesto.edu.nomoredomains.xyz',
+];
--- backend/utils/cors.js
+++ backend/utils/cors.js
@@ -1,7 +1,19 @@
-module.exports.cors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+const { allowedUrls } = require('./allowedUrls');
 
-  next();
+module.exports.cors = (req, res) => {
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const { origin } = req.headers;
+  const { method } = req;
+
+  if (allowedUrls.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+
+  return res.end();
 };

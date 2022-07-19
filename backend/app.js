fb118a4f65e284fa6e7d3b86e7de76cf18ee7f5a
Nikcet <mstislav.kr@yandex.ru>
2022-07-20 00:57:41 +0300

    Cors

--- backend/app.js
+++ backend/app.js
@@ -22,15 +22,16 @@ async function mongoInit() {
   await mongoose.connect('mongodb://localhost:27017/mestodb');
 }
 
+app.use(cors({
+  credentials: true,
+}));
+
 mongoInit().catch((err) => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
-app.use(cors({
-  credentials: true,
-}));
 
 app.use(requestLogger);
 

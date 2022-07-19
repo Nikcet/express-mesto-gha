f89950ab16172e3eeba4c2aa7500ef786bf54b6f
Nikcet <mstislav.kr@yandex.ru>
2022-07-19 20:02:30 +0300

    -

--- backend/utils/cors.js
+++ backend/utils/cors.js
@@ -1,11 +1,15 @@
+/* eslint-disable no-console */
 /* eslint-disable consistent-return */
 const { allowedUrls } = require('./allowedUrls');
 
 module.exports.cors = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
+  console.log(requestHeaders);
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const { origin } = req.headers;
+  console.log(origin);
   const { method } = req;
+  console.log(method);
 
   if (allowedUrls.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
